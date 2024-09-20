b3b4e98d97a0a1239976532100641c95ad67c7c7
alex <windev@nate.com>
2024-09-20 11:06:33 +0900

    초기 커밋.

---
+++ Derived.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsolePro
+{
+	class Base
+	{
+		protected string Name;
+
+		public Base(string Name)
+		{
+			this.Name = Name;
+			Console.WriteLine($"{this.Name}.Base()"); 
+		}
+
+		~Base()
+		{
+			Console.WriteLine($"{this.Name}.~Base()"); 
+		}
+
+		public void BaseMethod()
+		{
+			Console.WriteLine($"{Name}.BaseMethod()"); 
+		}
+	}
+
+	class Derived : Base 
+	{
+		public Derived(string Name) : base(Name)
+		{
+			Console.WriteLine($"{this.Name}.Derived()");
+		}
+
+		~Derived()
+		{
+			Console.WriteLine($"{this.Name}.~Derived()");
+		}
+
+		public void ProgramMethod()
+		{
+			Console.WriteLine($"{Name}.DerivedMethod()"); 
+		}
+
+		static void Main(string[] args)
+		{
+			Base a = new Base("a");
+			a.BaseMethod();
+
+			Derived b = new Derived("b"); 
+			b.BaseMethod();
+			b.ProgramMethod();			
+		}
+	}
+}
