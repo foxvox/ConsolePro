0dcaf4ca3f2c2cfe1196687bec6d6ac891e7b80f
alex <windev@nate.com>
2024-10-06 23:06:28 +0900

    record with

--- MainApp.cs
+++ MainApp.cs
@@ -17,7 +17,8 @@
 		static void Main(string[] args)
 		{
 			RTransaction tr1 = new RTransaction { From = "Alice", To = "Bob", Amount = 100 };
-			RTransaction tr2 = new RTransaction { From = "Alice", To = "Charlie", Amount = 100 };
+			RTransaction tr2 = tr1 with { To = "Charlie" };
+			RTransaction tr3 = tr2 with { From = "Dave", Amount = 30 }; 
 
 			Console.WriteLine(tr1); 
 			Console.WriteLine(tr2); 
